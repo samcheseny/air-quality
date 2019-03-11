44e169837685b3621abf644200cf05b0bfcfbe8b
Samuel Ndara <ndarasam@gmail.com>
2019-03-11 19:39:47 +0300

    Added the main project file

---
+++ v1/src/views/main/Main.vue
@@ -0,0 +1,5 @@
+<template src="./main.html"></template>
+<script src="./main.js"></script>
+<style scoped lang="scss">
+    @import "./main";
+</style>
---
+++ v1/src/views/main/main.js
@@ -0,0 +1,14 @@
+import json from '../../data/english';
+
+export default {
+    name: 'Main',
+    data() {
+        return {
+            englishData: json
+        }
+    },
+    props: {},
+    mounted() {
+
+    }
+}
